0ed41f24ce89f920ff8836075e144d55e2892077
YATIN <yatin88617@gmail.com>
2020-01-26 23:34:57 +0530

    added an onClickListener and attached listView to BookAdapter

--- app/src/main/java/com/example/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/booklistingapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +14,12 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getName();
     private static EditText edit;
     private static ProgressBar spinner;
     private static ListView listView;
@@ -37,7 +42,36 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
         boolean isConnected = networkInfo!=null && networkInfo.isConnected();
 
+        if(!isConnected){
+            textView.setVisibility(View.VISIBLE);
+            edit.setVisibility(View.INVISIBLE);
+            button.setVisibility(View.INVISIBLE);
+        }else{
 
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String query = edit.getText().toString();
+                    query.trim().replaceAll("\\s+","");
+                    StringBuilder sb = new StringBuilder("https://www.googleapis.com/books/v1/volumes?q=");
+                    sb.append(query);
+                    sb.append("&maxResults=40");
+                    try{
+                        URL url = new URL(sb.toString());
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    listView = (ListView) findViewById(R.id.list);
+                    BookAdapter bookAdapter = new BookAdapter(MainActivity.this,new ArrayList<Book>());
+                    listView.setAdapter(bookAdapter);
+
+                }
+            });
+
+
+        }
 
     }
 }
