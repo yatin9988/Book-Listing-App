bd43388dfdf341ccb0377d97bd81fb0dad26a3ff
YATIN <yatin88617@gmail.com>
2020-01-26 19:03:18 +0530

    hooked up XML views in java code

--- app/src/main/java/com/example/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/booklistingapp/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.booklistingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static EditText edit;
+    private static ProgressBar spinner;
+    private static ListView listView;
+    private static TextView textView;
+    private static Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = (EditText) findViewById(R.id.edit_text);
+        spinner = (ProgressBar) findViewById(R.id.spinner);
+        listView = (ListView) findViewById(R.id.list);
+        textView = (TextView) findViewById(R.id.no_internet);
+        button = (Button) findViewById(R.id.search);
     }
 }
