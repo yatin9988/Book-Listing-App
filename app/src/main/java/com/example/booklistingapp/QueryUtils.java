80df7266bb40f76cff54fb8efd29c6562ae49eb2
YATIN <yatin88617@gmail.com>
2020-01-27 19:05:58 +0530

    reading input from an inputstream and converting it to a string

--- app/src/main/java/com/example/booklistingapp/QueryUtils.java
+++ app/src/main/java/com/example/booklistingapp/QueryUtils.java
@@ -3,6 +3,7 @@ package com.example.booklistingapp;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -68,8 +69,26 @@ public class QueryUtils {
         }
 
     }
-    
 
+    public static String readFromInputStream(InputStream inputStream){
+
+        StringBuilder sb = new StringBuilder();
+        try{
+
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = bufferedReader.readLine();
+            while(line!=null){
+                sb.append(line);
+                line = bufferedReader.readLine();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return sb.toString();
+        
+    }
 
 
 }
